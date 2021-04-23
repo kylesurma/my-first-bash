6caa2a97997bf65424e310f3567bdbe036f11923
Hernan Berisso <hberisso1@gmail.com>
2021-04-23 15:10:29 -0400

    added pwd clone functionality

--- bash.js
+++ bash.js
@@ -1,8 +1,14 @@
 process.stdout.write('prompt > ');
 process.stdin.on('data', (data) => {
+const pwd = data.toString().trim(); 
+if (pwd === 'pwd') {
+    
+    process.stdout.write(__dirname);
+    process.stdout.write('\nprompt > ');
+  } else {
   const cmd = data.toString().trim();
-
   process.stdout.write('you typed: ' + cmd);
   process.stdout.write('\nprompt > ');
+}
 
 })
