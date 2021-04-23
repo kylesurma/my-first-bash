f9abc73a561cf7feef83ff4a47ff973cdef0a664
Kyle Surma <surmakyl@gmail.com>
2021-04-23 15:12:04 -0500

    more manual labor

--- bash.js
+++ bash.js
@@ -1,14 +1,4 @@
-process.stdout.write('prompt > ');
-process.stdin.on('data', (data) => {
-const pwd = data.toString().trim(); 
-if (pwd === 'pwd') {
-    
-    process.stdout.write(__dirname);
-    process.stdout.write('\nprompt > ');
-  } else {
-  const cmd = data.toString().trim();
-  process.stdout.write('you typed: ' + cmd);
-  process.stdout.write('\nprompt > ');
-}
+const pwd = require('./pwd')
 
-})
+pwd()
+
